056aad9faed8318871b790d51ac8b101e9f83e96
zenglen42 <gsnospampls@pobox.com>
2016-11-09 13:34:51 -0600

    find the missing card in a deck of 52

--- week-03/missingCard/missingCard.js
+++ week-03/missingCard/missingCard.js
@@ -1,54 +1,9 @@
+
+// Get random deck from:
 // http://www.overclock.net/t/1517621/objects-and-cards-in-javascript
 
-function makeDeck() {
-    var ranks = new Array("A", "2", "3", "4", "5", "6", "7", "8", "9", "10",
-                    "J", "Q", "K");
-    var suits = new Array("Clubs", "Diamonds", "Hearts", "Spades");
-    
-    this.deck = new Array(52);
-    
-    var i, j;
-    for (i = 0; i < 4; i++) {
-        for (j = 0; j < 13; j++) {
-            this.deck[i*ranks.length + j] = new Card(ranks[j], suits[i]);
-        }
-    }
-}
-
-function shuffle() {
-    var i, j, temp;
-    var n = 10;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < this.deck.length; j++) {
-            k = Math.floor(Math.random() * this.deck.length);
-            temp = this.deck[j];
-            this.deck[j] = this.deck[k];
-            this.deck[k] = temp;
-        }
-    }
-}
-
-function Deck() {
-    
-    this.deck = new Array();
-    
-    this.makeDeck = makeDeck;
-    this.shuffle = shuffle;
-}
-
-function Card (rank, suit) {
-    this.rank = rank;
-    this.suit = suit;
-}
-
-var deck = new Deck();
-deck.makeDeck();
-deck.shuffle();
-console.log(deck);
-
-/*
-
-var deck = [ { rank: '7', suit: 'Diamonds' },
+var deck = [ 
+     { rank: '7', suit: 'Diamonds' },
      { rank: '6', suit: 'Spades' },
      { rank: '2', suit: 'Clubs' },
      { rank: '10', suit: 'Spades' },
@@ -98,7 +53,29 @@ var deck = [ { rank: '7', suit: 'Diamonds' },
      { rank: '8', suit: 'Clubs' },
      { rank: 'Q', suit: 'Hearts' },
      { rank: 'Q', suit: 'Spades' },
-     { rank: '9', suit: 'Clubs' },
+     /*{ rank: '9', suit: 'Clubs' },*/
      { rank: '9', suit: 'Spades' } ];
 
-*/
+var freq = {};
+
+for (var i = 0; i < deck.length; i++) {
+    var rank = deck[i].rank;
+    var suit = deck[i].suit;
+    if (freq[rank] === undefined) {
+        freq[rank] = 1;
+    } else {
+        freq[rank]++;
+        if (freq[rank] == 4) {
+            delete freq[rank];
+        }
+    }
+    if (freq[suit] === undefined) {
+        freq[suit] = 1;
+    } else {
+        freq[suit]++;
+        if (freq[suit] == 13) {
+            delete freq[suit];
+        }
+    }
+}
+console.log("This missing card is:", freq);
